ce0bdeeca7133bb80a0810066ac61e4b5e91516a
afsharfeshari-code <afsharfeshari@gmail.com>
2026-02-16 16:25:26 +0330

    main.py

--- main.py
+++ main.py
@@ -11,12 +11,8 @@ def home():
 # ---------- اجرای ربات در پس‌زمینه ----------
 def run_bot():
     import old_bot  # نام فایل رباتت بدون .py
-    # اگر old_bot تابع main دارد، فعال کن:
-    # old_bot.main()
+    old_bot.main()
 
 if __name__ == "__main__":
-    # اجرای ربات در یک ترد جداگانه
     threading.Thread(target=run_bot).start()
-    
-    # اجرای وب سرور Flask روی پورت 8080
     app.run(host="0.0.0.0", port=8080)
